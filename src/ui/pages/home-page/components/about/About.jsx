ee91c3ce332c13429686384c9e66cc848e1e83e1
mdlpontual <mdlpontual@gmail.com>
2025-04-15 20:39:01 -0300

    end of the day commit - chagnge the design for the about section

--- src/ui/pages/home-page/components/about/About.jsx
+++ src/ui/pages/home-page/components/about/About.jsx
@@ -1,19 +1,33 @@
 import React from "react";
+import IMG from "../../../../../assets/imagesHub";
 
 function About() {
   return (
     <>
       <section id="logoCon" className="container-fluid">
         <div id="logoRow" className="row">
-          <h1>Logo</h1>
+          <div id="logoCol" className="col">
+            <img src={IMG.voltaEsLogo_Blue} alt="Volta Logo" />
+          </div>
         </div>
       </section>
-      <article id="aboutCon" className="container-fluid">
+      <article id="aboutCon" className="container-fluid" style={{backgroundImage: `url(${IMG.aboutBackground})`}}>
         <div id="aboutRow" className="row">
-          <h1>About</h1>
+          <div id="aboutCol" className="col">
+            <div id="aboutVoltaRow" className="row">
+              <div id="aboutVolta" className="col">
+                
+              </div>
+            </div>
+            <div id="aboutSustRow" className="row">
+              <div id="aboutSust" className="col">
+
+              </div>
+            </div>
+          </div>
         </div>
         <div id="videoRow" className="row">
-          <h1>Video</h1>
+          
         </div>
       </article>
     </>
