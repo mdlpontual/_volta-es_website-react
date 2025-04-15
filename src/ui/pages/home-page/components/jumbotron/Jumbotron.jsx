97fe6f1d61f6d6642a3ff6d6075f0a5914a65087
mdlpontual <mdlpontual@gmail.com>
2025-04-15 11:17:54 -0300

    jumbotron dev

--- src/ui/pages/home-page/components/jumbotron/Jumbotron.jsx
+++ src/ui/pages/home-page/components/jumbotron/Jumbotron.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import IMG from "../../../../../assets/imagesHub";
 
 function Jumbotron() {
   return (
     <>
-      <section id="jumboCon" className="container-fluid">
+      <section id="jumboCon" className="container-fluid" style={{backgroundImage: `url(${IMG.jumbotronBackground})`}}>
         <div id="jumboRow" className="row">
-          <h1>Jumbotron</h1>  
+          <div id="jumboColMenu" className="col">
+            <div id="menuIconRow" className="row">
+              <div id="menuIcon" className="col-auto">
+                <img src={IMG.menuIcon} alt="ícone Menu" />
+              </div>
+            </div>
+          </div>
+          <div id="jumboColText" className="col">
+            <div id="voltaJumboLogo" className="row">
+            
+            </div>
+            <div id="jumboText" className="row">
+            
+            </div>
+        </div>
         </div>
       </section>
     </>
