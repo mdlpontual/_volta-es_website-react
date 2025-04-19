dee2f3fa3ff5eddf2cfd63be86ebf63613250a00
mdlpontual <mdlpontual@gmail.com>
2025-04-19 18:44:18 -0300

    sustentabildiade text

--- src/ui/pages/home-page/components/about/About.jsx
+++ src/ui/pages/home-page/components/about/About.jsx
@@ -43,8 +43,39 @@ function About() {
         <div id="sustRow" className="row">
           <div id="sustCol" className="col">
             <div id="sustTextRow" className="row">
-              <div id="sustTextRow" className="row">
-
+              <div id="sustTextCol" className="col">
+                <div id="topTextRow" className="row">
+                  <p>
+                    <strong>SUSTENTABILIDADE</strong> é a qualidade de um sistema cujo uso dos recursos no presente não pode comprometer a disponibilidade dos mesmos para as gerações futuras.
+                    A sustentabilidade também pode ser definida como a capacidade do ser humano interagir com o mundo ao seu redor, 
+                    preservando o meio ambiente para não comprometer os recursos naturais das gerações futuras. 
+                    O conceito abrangente de sustentabilidade deve ter a capacidade de integrar as questões sociais, materiais, energéticas, 
+                    econômicas e ambientais.
+                  </p>
+                </div>
+                <div id="bulletPointsRow" className="row">
+                  <div id="bulletPointsCol" className="col-auto col-md">
+                    <p>
+                      <strong>Social</strong> – O ser humano é a parte mais importante do meio ambiente, em toda escala de tempo, 
+                      o Homem do presente assim como o do futuro.
+                      <br /><br />
+                      <strong>Materiais</strong> – Reduzir continuamente o uso de materiais extraídos da natureza, 
+                      substituir sempre que possível por produtos reciclados e/ou renováveis.
+                      <br /><br />
+                      <strong>Energia</strong> – Reduzir continuamente o uso de energias de fontes extraídas da natureza, finitas, 
+                      substituindo-as por fontes renováveis.
+                    </p>
+                  </div>
+                  <div id="bulletPointsCol" className="col-auto col-md">
+                    <p>
+                      <strong>Economia</strong> – Todas as atividades precisam ser economicamente viáveis ao longo do tempo, ou seja, 
+                      gerar riqueza de forma consistente para os investidores (lucro), para o estado (impostos) e para a sociedade (empregos).
+                      <br /><br />
+                      <strong>Ambiental</strong> – Respeitar o meio onde as atividades são desenvolvidas, não interferir negativamente seja no ambiente, 
+                      na comunidade, na atmosfera, etc...
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
