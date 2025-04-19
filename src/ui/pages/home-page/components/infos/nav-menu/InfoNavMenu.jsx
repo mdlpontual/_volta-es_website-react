b5a0fe7418c1320dc48a313088f2f5ae4c96c4bf
mdlpontual <mdlpontual@gmail.com>
2025-04-19 20:29:07 -0300

    nav buttons base

--- src/ui/pages/home-page/components/infos/nav-menu/InfoNavMenu.jsx
+++ src/ui/pages/home-page/components/infos/nav-menu/InfoNavMenu.jsx
@@ -1,17 +1,33 @@
 import React from "react";
+import IMG from "../../../../../../assets/imagesHub";
 
 function InfoNavMenu() {
   return (
     <>
       <div id="navButtonsRow" className="row">
         <div id="etapasCol" className="col">
-
+          <div id="buttonTextRow" className="row">
+            <h3><strong>ETAPAS <br /> DO PROJETO</strong></h3>
+          </div>
+          <div id="buttonIconRow" className="row">
+            <img src={IMG.phasesIcon_White} id="etapasIcon" alt="icone das etapas do projeto" />
+          </div>
         </div>
         <div id="comoFuncionaCol" className="col">
-
+          <div id="buttonTextRow" className="row">
+            <h3><strong>COMO FUNCIONA <br /> UM GERADOR FOTOVOLTAICO?</strong></h3>
+          </div>
+          <div id="buttonIconRow" className="row">
+            <img src={IMG.panelIcon_White} id="comoFuncionaIcon" alt="icone das etapas do projeto" />
+          </div>
         </div>
         <div id="nossosProjetosCol" className="col">
-
+          <div id="buttonTextRow" className="row">
+              <h3><strong>NOSSOS PROJETOS <br /> EXECUTADOS</strong></h3>
+            </div>
+            <div id="buttonIconRow" className="row">
+              <img src={IMG.projectsIcon_White} id="nossosProjetosIcon" alt="icone das etapas do projeto" />
+            </div>
         </div>
       </div>  
     </>
