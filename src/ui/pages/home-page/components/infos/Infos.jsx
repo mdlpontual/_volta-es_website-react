7be9d90622c1f6372168f58a8527c2d4466102c0
mdlpontual <mdlpontual@gmail.com>
2025-04-20 20:30:48 -0300

    info second section

--- src/ui/pages/home-page/components/infos/Infos.jsx
+++ src/ui/pages/home-page/components/infos/Infos.jsx
@@ -13,7 +13,32 @@ function Infos() {
           </div>
         </div>
         <div id="howItWorksRow" className="row">
-          <h1>Como Funciona</h1>
+          <div id="howItWorksCol" className="col">
+            <div id="howItWorksTextCon" className="container-fluid">
+              <div id="howItWorksTextRow" className="row">
+                <div id="howItWorksTextCol" className="col">
+                    <h4><strong id="tallCase">COMO FUNCIONA UM GERADOR FOTOVOLTAICO?</strong></h4>
+                    <p>
+                    O tipo de gerador solar mais difundido é o gerador fotovoltaico. 
+                    O termo fotovoltaico se aplica aos materiais que tem a capacidade de criar uma tensão elétrica 
+                    ao ser exposto a luz.
+                    Um gerador solar fotovoltaico é um equipamento simples, composto de módulos fotovoltaicos e um inversor. 
+                    Os módulos geram uma corrente elétrica ao receber a luz do sol. O inversor recebe esta corrente elétrica 
+                    e transformando-a para ser usada em nossa casa, com características iguais a 
+                    corrente elétrica da rede pública. Durante o dia, a energia consumida pela sua casa será gerada pelo 
+                    gerador fotovoltaico, e ao final da tarde,sua casa volta a receber energia elétrica da rede pública!
+                    </p>
+                </div>
+                <div id="howItWorksVideoCol" className="col">
+                  <iframe src="https://www.youtube.com/embed/F3gDyuhZUIM?si=Kq8-wbGu0u-a2pmp" 
+                        title="YouTube video player" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                  </iframe>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div id="letsTalkRow" className="row">
           <h1>Vamos Conversar</h1>
