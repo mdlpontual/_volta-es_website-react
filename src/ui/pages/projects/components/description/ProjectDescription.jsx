77bdc9f8143da784bac4b64a729d21c94366280a
mdlpontual <mdlpontual@gmail.com>
2025-04-25 18:33:23 -0300

    gallery body

--- src/ui/pages/projects/components/description/ProjectDescription.jsx
+++ src/ui/pages/projects/components/description/ProjectDescription.jsx
@@ -1,34 +1,4 @@
-/* import React from "react";
-import projectsData from "../../ProjectsDataHub";
-
-function ProjectDescription({ data }) {
-  return (
-    <>
-      <section id="prjDescCon" className="container-fluid">
-        <div id="projsubtitleRow" className="row">
-          <h2><strong>{data.subtitle}</strong></h2>
-        </div>
-        <div id="projDescRow" className="row">
-          <div id="projProfileCol" className="col">
-            <h4><strong>{data.title}</strong></h4>
-            <h4>{data.subtitle}</h4>
-            <h4><strong>Ano: </strong>{data.year}</h4>
-            <h4><strong>Módulos Instalados: </strong>{data.modules}</h4>
-            <h4><strong>Potência Gerada: </strong>{data.powerOutput}</h4>
-          </div>
-          <div id="projDescCol" className="col">
-            <h4>{data.description}</h4>  
-          </div>
-        </div>
-      </section>
-    </>
-  );
-};
-
-export default ProjectDescription; */
-
 import React from "react";
-import projectsData from "../../ProjectsDataHub";
 
 function ProjectDescription({ data }) {
   return (
--- src/ui/pages/projects/components/gallery-body/ProjectGallery.jsx
+++ src/ui/pages/projects/components/gallery-body/ProjectGallery.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
-function ProjectGallery() {
-  return (
-    <>
-      
-    </>
-  );
+function ProjectGallery({ data }) {
+    const imagesArr = Object.values(data.images);
+
+    return (
+        <> 
+            <section id="galleryCon" className="container-fluid">
+                {imagesArr.map((img, i) => (
+                    <div id="imgRow" className="row">
+                        <div id="imgCol" className="col-auto">
+                            <img src={img} alt="Imagem do projeto"/>
+                        </div>
+                    </div>
+                ))}
+            </section>
+        </>
+    );
 };
 
 export default ProjectGallery;
