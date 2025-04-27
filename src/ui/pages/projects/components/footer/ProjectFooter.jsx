c5f56c6ab8acc8da281138b6e9e26b8d7fc92c6d
mdlpontual <mdlpontual@gmail.com>
2025-04-27 18:03:21 -0300

    images height and bottom nav buttom

--- src/ui/pages/projects/components/footer/ProjectFooter.jsx
+++ src/ui/pages/projects/components/footer/ProjectFooter.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import IMG from "../../../../../assets/imagesHub";
+import { Link } from "react-router-dom";
 import ContactFooter from "../../../home-page/components/footer/contacts/ContactFooter";
 
 function ProjectFooter() {
   return (
     <>
       <section id="ppFooterCon" className="container-fluid">
-        <div id="partnersRow" className="row">
-
+        <div id="bottomNavRow" className="row">
+          <div id="homeNavCol" className="col-auto">
+            <Link to="/"><h4>Voltar a Página Inicial</h4></Link>
+          </div>
         </div>
         <ContactFooter />
       </section>
